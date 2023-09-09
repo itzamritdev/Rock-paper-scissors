fce300e4f1d70ba386c1756f9cea1737bb03df85
Amrit <amritpal.alampur@gmail.com>
2023-09-09 14:48:16 +0200

    created function to play a round added return statements if the player lose

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ function getComputerChoice() {
         return "Paper";
     }
     else {
-        return "Scissor";
+        return "Scissors";
+    }
+}
+// make a function that return a string based on the selection
+function round(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You lose! Paper beats Rock";
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You lose! Scissors beats Paper";
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "You lose! Rock beats scissors";
+    }
+    else {
+        return "You win! Congrats!";
     }
 }
